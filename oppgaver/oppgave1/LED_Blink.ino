30e125bbb9b6b28a319e0188eed53e182416b0c5
Håvard Opheim <haavard@vaage.com>
2018-08-21 11:48:17 +0200

    Add skeleton and solution to task 1

---
+++ oppgaver/oppgave1/LED_Blink.ino
@@ -0,0 +1,20 @@
+// Use this variable in your code
+// Remember to use this pin on tour board!
+const int ledPin = D7; 
+
+void setup() {
+  // put your setup code here, to run once:
+  // Here we should declare whitch pins we are using!
+
+  /* INSERT CODE HERE */ 
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Actual logic for blinking light goes here
+
+  /* INSERT CODE HERE */
+
+}
