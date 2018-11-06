59cc53f673bdb7dfc8ce94c655d353ad7ed02b01
Håvard Opheim <haavarop@stud.ntnu.no>
2018-11-06 20:15:31 +0100

    Update AutomatedShutter.ino

--- oppgaver/oppgave4/AutomatedShutter.ino
+++ oppgaver/oppgave4/AutomatedShutter.ino
@@ -2,49 +2,32 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
+// Bruk D6 og D7 portene på brettet dit
+const int openShutterPin = D7;
+const int closeShutterPin = D6;  
+
 char auth[] = "YOUR AUTH TOKEN";
 
 // Your WiFi credentials.
 char ssid[] = "Hackerspace Kurs"; // Your mobile phone must be on the same network
 char pass[] = ""; //No password is required
 
+//Denne koden kjøres når du trykker på knappen i telefonen din
+BLYNK_WRITE(V1) {
+  int pinValue = param.asInt();
+  // Bruk det du lærte i oppgave 2 og 3 til å løse denne! 
+  
+  /* Din kode her ! */
+  
+}
+
+
 void setup() {
-
+  pinMode(openShutterPin, OUTPUT);
+  pinMode(closeShutterPin, OUTPUT);
   Blynk.begin(auth, ssid, pass);
-
-  /* INSERT CODE HERE */ 
-
 }
 
 void loop() {
   Blynk.run();
 }
-
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-
-  You can use this sketch as a debug tool that prints all incoming values
-  sent by a widget connected to a Virtual Pin 1 in the Blynk App.
-
-  App project setup:
-    Slider widget (0...100) on V1
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
