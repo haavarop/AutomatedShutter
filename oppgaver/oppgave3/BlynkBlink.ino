daf865b24e0a590ca9aef6789bf805cfbda4af85
Håvard Opheim <haavarop@stud.ntnu.no>
2018-11-06 20:00:43 +0100

    Update BlynkBlink.ino

--- oppgaver/oppgave3/BlynkBlink.ino
+++ oppgaver/oppgave3/BlynkBlink.ino
@@ -8,11 +8,24 @@ const int ledPin = D7;
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "YOUR AUTH TOKEN";
+char auth[] = "YOUR AUTH TOKEN"; //Husk å put auth-token din her
 
 // Your WiFi credentials.
-char ssid[] = "Hackerspace Kurs"; // Your mobile phone must be on the same network
-char pass[] = ""; //No password is required
+char ssid[] = "Hackerspace Kurs"; // Mobilappen din må være på dette nettet!
+char pass[] = ""; 
+
+// Her får vi kode fra Blynk
+// Den blir kalt hver gang vi trykker på knappen i appen 
+BLYNK_WRITE(V1) {
+  int pinValue = param.asInt(); //Her lagrer vi verdien som kommer fra BLYNK
+  // pinValue kan ha to verdier 0 eller 1
+  if(pinValue == 1) {
+    // Putt kode her for å skru på lyset
+  }
+  
+  // Du trenger også kode for å skru av l
+  
+}
 
 void setup() {
   // Debug console
@@ -21,39 +34,9 @@ void setup() {
   // Starts Blynk
   Blynk.begin(auth, ssid, pass);
 
-  /* INSERT CODE HERE */ 
-
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   Blynk.run();
 }
-
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-
-  You can use this sketch as a debug tool that prints all incoming values
-  sent by a widget connected to a Virtual Pin 1 in the Blynk App.
-
-  App project setup:
-    Slider widget (0...100) on V1
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
